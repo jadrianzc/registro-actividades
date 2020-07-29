2a2f330dced2b842aa3222db85f8ee9bc44017f0
Adrian Zambrano <jadrianzc99@gmail.com>
2020-07-28 21:41:15 -0500

    Actualización

--- frontend/service/service.js
+++ frontend/service/service.js
@@ -3,18 +3,21 @@ class TrabajoService {
         this.URI = '/api/trabajos';
     }
 
+    // Método para acceder a la API REST del backend y obtener todos los datos
     async getTrabajo() {
         const res = await fetch(this.URI);
         const trabajos = await res.json();
         return trabajos;
     }
 
+    // Método para acceder a la API REST del backend y obtener un sólo los datos, recibe como parámetro el código de la tarea (id)
     async getTrabajoOne(trabajoId) {
         const res = await fetch(`${this.URI}/${trabajoId}`);
         const trabajos = await res.json();
         return trabajos
     }
 
+    // Método para acceder a la API REST del backend y enviar un nuevo dato
     async postTrabajo(trabajo) {
         const data = JSON.stringify(trabajo);
         const res = await fetch(this.URI, {
@@ -28,6 +31,7 @@ class TrabajoService {
         return trabajos.message.toUpperCase();
     }
 
+    // Método para acceder a la API REST del backend y actualizar un dato
     async putTrabajo(trabajo) {
         const data = JSON.stringify(trabajo);
         const res = await fetch(this.URI, {
@@ -41,6 +45,7 @@ class TrabajoService {
         return trabajos.message.toUpperCase();
     }
 
+    // Método para acceder a la API REST del backend y eliminar un dato, recibe como parámetro el código de la tarea (id)
     async deleteTrabajo(trabajoId) {
         const res = await fetch(`${this.URI}/${trabajoId}`, {
             headers: {
